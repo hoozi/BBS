1d56a12c001e5a43a3991f5894e6d1b7131910da
AngryPanda <287036406@qq.com>
2017-03-03 20:47:12 +0800

    add Topic actor

--- core/Topic.js
+++ core/Topic.js
@@ -9,11 +9,45 @@ class Topic extends Actor {
             author: data.author,
             title: data.title,
             content: data.content,
+            accessNum: 0,
             top: false,//置顶
             fine: false,//加精
             createTime: Date.now(),
             updateTime: Date.now()
         });
     }
-
+    top(data, service) {
+        service.apply('top');
+    }
+    untop(data, service) {
+        service.apply('untop');
+    }
+    fine(data, service) {
+        service.apply('fine');
+    }
+    unfine(data, service) {
+        service.apply('unfine');
+    }
+    access(data, service) {
+        service.apply('access')
+    }
+    when(event) {
+        switch(event.name) {
+            case 'top':
+                this._data.top = true;
+            break;
+            case 'uptop':
+                this._data.top = false;
+            break;
+            case 'fine':
+                this._data.fine = true;
+            break;
+            case 'unfine':
+                this._data.unfine = false;
+            break;
+            case 'access':
+                ++this._data.accessNum;
+            break;
+        }
+    }
 }
